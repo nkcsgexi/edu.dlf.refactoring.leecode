c90084b559cab7776a7a9cab0da2468cb0dcb9cb
xi ge <xge@ncsu.edu>
2014-05-20 22:36:56 -0400

    pie

--- edu.dlf.refactoring.leecode/src/edu/dlf/refactoring/leecode/GeometryProblems.java
+++ edu.dlf.refactoring.leecode/src/edu/dlf/refactoring/leecode/GeometryProblems.java
@@ -11,4 +11,18 @@ public class GeometryProblems {
 		Point p4) {
 		return !(p2.y < p3.y || p1.y > p4.y || p4.x < p1.x || p2.x < p3.x) ;
 	}
+	
+	private static double calculatePie() {
+		int width = 10000;
+		double fanSpace = 0;
+		for(int i = 1; i < width; i ++) {
+			double s = width * width - i * i;
+			fanSpace += Math.pow(s, 0.5);
+		}
+		return 4 * fanSpace / (width * width);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(calculatePie());
+	}
 }
