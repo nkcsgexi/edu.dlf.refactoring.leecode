849e3ae28328882049c5a6274a3972d224e261ac
Xi Ge <xge@ncsu.edu>
2014-05-21 21:01:39 -0400

    pre in traversal of trees

--- edu.dlf.refactoring.leecode/src/edu/dlf/refactoring/leecode/GraphProblems.java
+++ edu.dlf.refactoring.leecode/src/edu/dlf/refactoring/leecode/GraphProblems.java
@@ -9,7 +9,7 @@ import java.util.function.Consumer;
 
 public class GraphProblems {
 	
-	private class GraphNode {
+	private static class GraphNode {
 		GraphNode(int value) {
 			this.value = value;
 		}
@@ -17,10 +17,33 @@ public class GraphProblems {
 		List<GraphNode> neighbors = new ArrayList<GraphNode>();
 	}
 	
-	private class BinaryTreeNode {
+	private static class BinaryTreeNode {
+		int value;
 		BinaryTreeNode left;
 		BinaryTreeNode right;
+		BinaryTreeNode(int value) {
+			this.value = value;
+		}
 	}
+	
+	private static void previstTree(BinaryTreeNode root, Consumer<BinaryTreeNode> visit) {
+		visit.accept(root);
+		if(root.left != null) previstTree(root.left, visit);
+		if(root.right != null) previstTree(root.right, visit);
+	}
+	
+	private static void postvisitTree(BinaryTreeNode root, Consumer<BinaryTreeNode> visit) {
+		if(root.left != null) postvisitTree(root.left, visit);
+		if(root.right != null) postvisitTree(root.right, visit);
+		visit.accept(root);	
+	}
+	
+	private static void invisitTree(BinaryTreeNode root, Consumer<BinaryTreeNode> visit) {
+		if(root.left != null) invisitTree(root.left, visit);
+		visit.accept(root);
+		if(root.right != null) invisitTree(root.right, visit);
+	}
+		
 	
 	private void depthFirstVisit(GraphNode head, Consumer<GraphNode> visit) {
 		Stack<GraphNode> visitedNode = new Stack<GraphNode>();
@@ -42,6 +65,7 @@ public class GraphProblems {
 		}
 	}
 	
+	
 	private GraphNode cloneGraph(GraphNode head) {
 		if(head.neighbors.isEmpty()) {
 			return new GraphNode(head.value);
@@ -60,6 +84,59 @@ public class GraphProblems {
 		BinaryTreeNode right = lowestCommonAncestor(root.right, node1, node2);
 		if(left != null && right != null) return root;
 		return left == null ? right : left;
+	}
+	
+	private static BinaryTreeNode createTreeByPreorderAndInorderTraversal(
+			List<Integer> inorder, List<Integer> preorder) {	
+		if(inorder.size() == 0) {
+			return null;
+		}
+		if(inorder.size() == 1) {
+			if(inorder.get(0) == preorder.get(0)) 
+				return new BinaryTreeNode(inorder.get(0));
+			else 
+				return null;
+		}
+		BinaryTreeNode root = new BinaryTreeNode(preorder.get(0));
+		for(int index = 0; index < inorder.size(); index ++) {
+			if(inorder.get(index) != preorder.get(0)) continue;
+			List<Integer> beforeIn = getSublist(inorder, 0, index);
+			List<Integer> afterIn = getSublist(inorder, index + 1, inorder.size());
+			List<Integer> beforePre = getSublist(preorder, 1, index + 1);
+			List<Integer> afterPre = getSublist(preorder, index + 1, preorder.size());
+			BinaryTreeNode left, right;
+			left = createTreeByPreorderAndInorderTraversal(beforeIn, beforePre);
+			right = createTreeByPreorderAndInorderTraversal(afterIn, afterPre);
+			if(left != null || beforeIn.isEmpty()) {
+				if(right != null || afterIn.isEmpty()) {
+					root.left = left;
+					root.right = right;
+					return root;
+				}
+			}	
+		}
+		return null;
+	}
+	
+	private static List<Integer> getSublist(List<Integer> list, int start, int end) {
+		return list.subList(start, end);
+	}
+	
+	private static List<Integer> toList(int[] array) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int a : array) {
+			list.add(a);
+		}
+		return list;
+	}
+	
+	public static void main(String[] args) {
+		List<Integer> preorder = toList(new int[]{7,10,4,3,1,2,8,11});
+		List<Integer> inorder = toList(new int[]{4,10,3,1,7,11,8,2});
+		BinaryTreeNode root = createTreeByPreorderAndInorderTraversal(inorder, 
+			preorder);
+		previstTree(root, r -> System.out.println(r.value));
+		invisitTree(root, r -> System.out.println(r.value));
 	}
 	
 	
