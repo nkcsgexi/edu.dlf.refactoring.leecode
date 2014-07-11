deea1dab7a747af5f5da8dfccf433e99b94bf979
xi ge <xge@ncsu.edu>
2014-07-11 10:33:34 -0400

    suduko solver

--- edu.dlf.refactoring.leecode/src/edu/dlf/refactoring/leecode/GameProblem.java
+++ edu.dlf.refactoring.leecode/src/edu/dlf/refactoring/leecode/GameProblem.java
@@ -1,5 +1,9 @@
 package edu.dlf.refactoring.leecode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class GameProblem {
 
 	/*
@@ -45,15 +49,15 @@ public class GameProblem {
 		... 
 
 		You can issue the following commands to move the cursor and select letters: 
-		‘u’ - up 
-		‘d’ - down 
-		‘l’ - left 
-		‘r’ - right 
-		‘!’ - select letter 
+		ï¿½uï¿½ - up 
+		ï¿½dï¿½ - down 
+		ï¿½lï¿½ - left 
+		ï¿½rï¿½ - right 
+		ï¿½!ï¿½ - select letter 
 
 		You are given an input string and the length of the rows in the on-screen 
 		keyboard. You must produce the sequence of commands needed to type out 
-		the input string on the specified keyboard, e.g.: “aci”, 5 -> “!rr!dr!”
+		the input string on the specified keyboard, e.g.: ï¿½aciï¿½, 5 -> ï¿½!rr!dr!ï¿½
 	*/	
 	private static void getMove(int N, String s ) {
 		int previousRow = 0;
@@ -89,10 +93,73 @@ public class GameProblem {
 		System.out.println("!");
 	}
 
+	private static void sudokuSolver(int[][] grid) {
+		// I assume it is 9 * 9
+		boolean leaf = true;
+		for(int i = 0; i < 9; i++){
+			for(int j = 0; j < 9; j++) {
+				if(grid[i][j] < 1 || grid[i][j] > 9) {
+					int original = grid[i][j];
+					List<Integer> numbers = 
+						findPossibleNumbers(grid, i, j);
+					for(int n : numbers) {
+						grid[i][j] = n;
+						sudokuSolver(grid);
+					}
+					grid[i][j] = original;
+					leaf = false;
+				}	
+			}
+		}
 
+		if(!leaf) 
+			return;
+		System.out.println("solutions:");
+		for(int i = 0; i < 9; i ++) {
+			for(int j = 0; j < 9; j ++) {
+				System.out.print(grid[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	private static List<Integer> findPossibleNumbers(int[][] grid, int i, int j) {
+		List<Integer> results = new ArrayList<Integer>();
+		HashSet<Integer> forbiddenNumbers = new HashSet<Integer>();
+		for(int k = 0; k < 9; k ++) {
+			forbiddenNumbers.add(grid[i][k]);
+			forbiddenNumbers.add(grid[k][j]);
+		}
+		int startRow = i / 3 * 3;
+		int startColumn = j / 3 * 3;
+		for(int r = startRow; r < startRow + 3; r ++)
+			for(int c = startColumn; c < startColumn + 3; c ++)
+				forbiddenNumbers.add(grid[r][c]);
+		for(int num = 1; num <= 9; num ++){
+			if(forbiddenNumbers.contains(num))
+				continue;
+			results.add(num);
+		}
+		return results;
+	}
+	
+	private static void sudokuSolverTest() {
+		int[][] grids = {
+				new int[]{7, 0, 0, 0, 2, 6, 0, 0, 8},
+				new int[]{4, 0, 0, 3, 5, 7, 0, 6, 0},
+				new int[]{0, 0, 6, 0, 1, 8, 0, 9, 0},
+				new int[]{0, 3, 0, 0, 9, 4, 7, 0, 0},
+				new int[]{9, 0, 0, 0, 0, 0, 0, 0, 4},
+				new int[]{0, 0, 2, 6, 7, 0, 0, 8, 0},
+				new int[]{0, 8, 0, 7, 6, 0, 9, 0, 0},
+				new int[]{0, 7, 0, 2, 3, 1, 0, 0, 5},
+				new int[]{5, 0, 0, 8, 4, 0, 0, 0, 3},
+		};
+		sudokuSolver(grids);
+	}
 	
 	public static void main(String[] args) {
-		getMove(5, "aci");
+		sudokuSolverTest();
 	}
 	
 }
